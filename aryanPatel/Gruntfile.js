761cf4dcae670f632b2afaf9aa0bf50960857655
aryan <aryan9664871107@gmail.com>
2019-12-03 20:56:37 -0500

    JavaScript

---
+++ aryanPatel/Gruntfile.js
@@ -0,0 +1,10 @@
+/* 
+ * Aryan Patel
+ *  Student Id:- 991511203
+ * SYS10199 - WEB PROGRAMMING 
+ */
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+    });
+};
---
+++ aryanPatel/gulpfile.js
@@ -0,0 +1,11 @@
+/* 
+ * Aryan Patel
+ *  Student Id:- 991511203
+ * SYS10199 - WEB PROGRAMMING 
+ */
+
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
